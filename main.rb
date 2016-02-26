983a3be70af490d29a419f320102a569ec228803
Bridget McMillan <bridgetmc09@gmail.com>
2016-02-26 14:13:35 -0500

    sign-in and sign-up functionality working

--- main.rb
+++ main.rb
@@ -4,34 +4,54 @@ require "sinatra/reloader"
 require "sqlite3"
 require "rake"
 require "sinatra/flash"
+require "./models"
 
 set :database, "sqlite3:MFB.db"
-
 
 get "/" do  
   erb :index
 end
 
-get "/home" do
+post "/" do
+  @user = User.where(handle: params[:username]).first
 
-    erb :home
+  if @user && @user.password == params[:password]
+    session[:user_id] = @user.id
+    redirect "/home"
+  end
+
+  erb :index
 end
 
+get "/home" do
+  erb :home
+end
 
 get "/sign-up" do
   erb :sign_up
 end
 
 post "/sign-up" do
+  if params[:password] == params[:ver_password]
+  
+    @user = User.create(
+      handle: params[:username],
+      password: params[:password],
+      email: params[:email],
+      fname: params[:fname],
+      lname: params[:lname],
+      birthday: params[:birthday],
+      station: params[:station],
+      bio: params[:bio]
+      )
 
-    redirect "/home"
+      @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "You're freakin' in!"
+      redirect "/home"
 
-  erb :sign_up
-end
-
-post "/index" do
-
-    redirect "/home"
-
-  erb :index
+    else
+      flash[:alert] = "Check your freakin' credentials. Does your ish match?"
+    end
+      erb :sign_up
 end
