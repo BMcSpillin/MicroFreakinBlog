fb88be95f47020cc7d18a8dda53c0f7b60751c81
Bridget McMillan <bridgetmc09@gmail.com>
2016-02-29 12:26:02 -0500

    edit is starting to work.  check this out,

--- models.rb
+++ models.rb
@@ -1,15 +1,13 @@
 class User < ActiveRecord::Base
   has_many :posts
-  before_update :destroy_current_user
-
-  def destroy_current_user
-    
-  end
 end
-
-
 
 class Post < ActiveRecord::Base
   belongs_to :user
   accepts_nested_attributes_for :user
 end
+
+# ActiveRecord::Base.establish_connection(
+#   :adapter => 'sqlite3',
+#   :database =>  'sinatra_application.sqlite3.db'
+# )
